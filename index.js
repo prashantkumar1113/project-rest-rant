aed2f562dc76042bf3cd2132a448ca0f370962f4
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-03-08 00:01:00 -0600

    added the 404 route

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
@@ -5,4 +7,11 @@ app.get("/", (req, res) => {
     res.send("Hello world!");
 });
 
-app.listen(3000);
+// 404 Page
+app.get("*", (req, res) => {
+    res.send(`<h1>404 Page</h1>`);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is on...");
+});
