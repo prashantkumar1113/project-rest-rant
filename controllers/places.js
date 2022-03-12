15c0d5e04f25d69aa8754d134bb2aafed63a152c
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-03-12 14:21:14 -0600

    Form data is getting through

--- controllers/places.js
+++ controllers/places.js
@@ -21,7 +21,8 @@ router.get("/", (req, res) => {
     res.render("places/index", {places});
 });
 router.post("/", (req, res) => {
-    res.send("Create new place");
+    console.log(req.body);
+    res.send("POST /places");
 });
 router.get("/new", (req, res) => {
     // res.send("Form page for creating a new place");
--- index.js
+++ index.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//MIDDLEWARE
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
 
 app.use("/places", require("./controllers/places"));
 
